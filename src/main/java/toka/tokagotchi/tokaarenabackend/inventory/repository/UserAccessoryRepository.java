47863d78c94d066d4a232cbb3bcff4cc12731bc2
Akko <131815569+Bumh3rr@users.noreply.github.com>
2026-04-09 01:20:47 -0600

    fix

--- src/main/java/toka/tokagotchi/tokaarenabackend/inventory/repository/UserAccessoryRepository.java
+++ src/main/java/toka/tokagotchi/tokaarenabackend/inventory/repository/UserAccessoryRepository.java
@@ -6,5 +6,6 @@ import toka.tokagotchi.tokaarenabackend.inventory.model.UserAccessory;
 import java.util.Optional;
 
 public interface UserAccessoryRepository extends JpaRepository<UserAccessory, Long> {
+	Optional<UserAccessory> findByAccessory_Id(long id);
 	Optional<UserAccessory> findByOwnerIdAndAccessoryId(Long ownerId, Long accessoryId);
 }
--- src/main/java/toka/tokagotchi/tokaarenabackend/tokagotchi/repository/TokagotchiRepository.java
+++ src/main/java/toka/tokagotchi/tokaarenabackend/tokagotchi/repository/TokagotchiRepository.java
@@ -1,6 +1,7 @@
 package toka.tokagotchi.tokaarenabackend.tokagotchi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import toka.tokagotchi.tokaarenabackend.inventory.model.UserAccessory;
 import toka.tokagotchi.tokaarenabackend.tokagotchi.model.Tokagotchi;
 
 import java.util.Optional;
